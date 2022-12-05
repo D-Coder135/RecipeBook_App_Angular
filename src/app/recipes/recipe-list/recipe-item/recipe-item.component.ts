dafe31b26f975d0f03cec39d607c4e2d53c587cd
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-12-05 17:55:43 +0530

    add the @Output decorator to make that property accessible globally

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {Recipe} from "../../recipe.model";
 
 @Component({
@@ -9,7 +9,7 @@ import {Recipe} from "../../recipe.model";
 export class RecipeItemComponent implements OnInit {
 
  @Input() recipeItem: Recipe;
- recipeSelected = new EventEmitter<void>();
+  @Output() recipeSelected = new EventEmitter<void>();
   constructor() { }
 
   ngOnInit(): void {
