9fd77e1551e1cb53e34d692a58517ad08fc8d1fa
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-12-22 15:21:33 +0530

    add a parameter private recipeService of type RecipeService inside the constructor()

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Recipe} from "../recipe.model";
+import {RecipeService} from "../recipe.service";
 
 @Component({
   selector: 'app-recipe-list',
@@ -9,9 +10,10 @@ import {Recipe} from "../recipe.model";
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[];
 
+  constructor(private recipeService: RecipeService) { }
+
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
-  constructor() { }
 
   ngOnInit(): void {
   }
