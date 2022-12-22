b52a329341dc0d9a6aed81cde1b810dd04dfc43c
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-12-22 15:33:02 +0530

    remove some code and optimize imports inside the recipe-item.component.ts

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {Recipe} from "../../recipe.model";
 
 @Component({
@@ -9,13 +9,12 @@ import {Recipe} from "../../recipe.model";
 export class RecipeItemComponent implements OnInit {
 
  @Input() recipeItem: Recipe;
-  @Output() recipeSelected = new EventEmitter<void>();
   constructor() { }
 
   ngOnInit(): void {
   }
 
   onSelected() {
-    this.recipeSelected.emit();
+
   }
 }
