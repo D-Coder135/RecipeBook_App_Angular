516ba67fcfc86ac5464a7b1819e95915993a8bfd
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-12-22 15:32:17 +0530

    initialize a property recipeSelected with the object EventEmitter<Recipe>

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,9 @@
+import { EventEmitter } from "@angular/core";
 import {Recipe} from "./recipe.model";
 
 export class RecipeService {
+
+  recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test recipe.', 'https://static.onecms.io/wp-content/uploads/sites/44/2022/03/01/cucumber-sandwich.jpg')
   ];
