6e85017e48ef3662cbf195d12f5cd41915ff42e3
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-12-22 23:28:06 +0530

    pass an empty array as a parameter

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -5,7 +5,8 @@ export class RecipeService {
 
   recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test recipe.', 'https://static.onecms.io/wp-content/uploads/sites/44/2022/03/01/cucumber-sandwich.jpg')
+    new Recipe('A Test Recipe', 'This is simply a test recipe.',
+      'https://static.onecms.io/wp-content/uploads/sites/44/2022/03/01/cucumber-sandwich.jpg', [])
   ];
 
 
