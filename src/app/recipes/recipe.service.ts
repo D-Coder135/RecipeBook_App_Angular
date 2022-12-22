62f72f8be2fb32abb09d64640a602f02128821cf
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-12-22 15:16:39 +0530

    paste the cut-out part of the code inside the recipe.service.ts

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,3 +1,7 @@
-export class RecipeService {
+import {Recipe} from "./recipe.model";
 
+export class RecipeService {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test recipe.', 'https://static.onecms.io/wp-content/uploads/sites/44/2022/03/01/cucumber-sandwich.jpg')
+  ];
 }
