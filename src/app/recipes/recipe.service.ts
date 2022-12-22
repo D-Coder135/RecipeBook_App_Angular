e075ba91d673a23ea0a694fe4d5a2b353252ed90
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-12-22 23:29:29 +0530

    initialize objects of type Ingredient inside the array

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,17 @@
 import { EventEmitter } from "@angular/core";
 import {Recipe} from "./recipe.model";
+import {Ingredient} from "../shared/ingredient.model";
 
 export class RecipeService {
 
   recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test recipe.',
-      'https://static.onecms.io/wp-content/uploads/sites/44/2022/03/01/cucumber-sandwich.jpg', [])
+      'https://static.onecms.io/wp-content/uploads/sites/44/2022/03/01/cucumber-sandwich.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ])
   ];
 
 
