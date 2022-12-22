645ca11332f635c9b2ece4cdaa8402dab4ce561d
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-12-22 15:18:53 +0530

    add a getter() method for the recipes array object

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -4,4 +4,9 @@ export class RecipeService {
   private recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test recipe.', 'https://static.onecms.io/wp-content/uploads/sites/44/2022/03/01/cucumber-sandwich.jpg')
   ];
+
+
+  getRecipes(): Recipe[] {
+    return this.recipes.slice();
+  }
 }
