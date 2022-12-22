b676267a39c9d815b3b8abd7e6f9f56dd2d28270
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-12-22 15:17:16 +0530

    add the private keyword

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,7 +1,7 @@
 import {Recipe} from "./recipe.model";
 
 export class RecipeService {
-  recipes: Recipe[] = [
+  private recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test recipe.', 'https://static.onecms.io/wp-content/uploads/sites/44/2022/03/01/cucumber-sandwich.jpg')
   ];
 }
